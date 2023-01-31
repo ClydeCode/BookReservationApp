dbc38089041cf34885af5961995bd077de73836f
Clyde <noneclyde@gmail.com>
2023-01-31 17:58:31 +0200

    Update contextDb

--- BookReservationApp/Data/ContextDb.cs
+++ BookReservationApp/Data/ContextDb.cs
@@ -1,14 +1,23 @@
 ﻿using BookReservationApp.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookReservationApp.Data;
 
-public class ContextDb : DbContext
+public class ContextDb : IdentityDbContext<UserModel>
 {
-    public ContextDb(DbContextOptions<ContextDb> options) 
+    public ContextDb(DbContextOptions<ContextDb> options)
         : base(options)
     {
     }
 
-    public DbSet<BookModel> Books { get; set; } = default!;
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
+    public DbSet<BookModel> Books { get; set; }
 }
