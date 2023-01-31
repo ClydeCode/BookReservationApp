bc6d8227a84f206d13f46c795fea16616cdb1efc
Clyde <noneclyde@gmail.com>
2023-01-31 17:58:02 +0200

    Add UserModel class

---
+++ BookReservationApp/Data/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookReservationApp.Data.Models
+{
+    public class UserModel : IdentityUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
