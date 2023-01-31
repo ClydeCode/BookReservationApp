b4c11235e620932c90847c4ae2ed5d7c994b6bf4
Clyde <noneclyde@gmail.com>
2023-01-31 15:41:22 +0200

    Add DbContext service

--- BookReservationApp/Program.cs
+++ BookReservationApp/Program.cs
@@ -1,9 +1,13 @@
+using BookReservationApp.Data;
+using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
+builder.Services.AddDbContextFactory<ContextDb>(opt => 
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbContext")));
 
 var app = builder.Build();
 
