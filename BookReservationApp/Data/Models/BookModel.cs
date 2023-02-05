64f1f263946a5a44c095b37d7e4eb61cf5c7aaac
Clyde <noneclyde@gmail.com>
2023-02-05 14:46:29 +0200

    Add ImageURL property to a BookModel

--- BookReservationApp/Data/Models/BookModel.cs
+++ BookReservationApp/Data/Models/BookModel.cs
@@ -5,6 +5,7 @@ namespace BookReservationApp.Data.Models;
 public class BookModel
 {
     public int Id { get; set; }
+    public string? ImageURL { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
     [ForeignKey("User")]
